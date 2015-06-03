943fe5ee85b27d50a9f10acbece7ff844453ed63
hectorbenitez <hbenitez@nearsoft.com>
2015-06-02 20:04:05 -0500

    Testing ProxyFix

--- hello.py
+++ hello.py
@@ -1,8 +1,10 @@
 import os
 from flask import Flask
 from flas.decorators import ssl_required
+from werkzeug.contrib.fixers import ProxyFix
 
 app = Flask(__name__)
+app.wsgi_app = ProxyFix(app.wsgi_app)
 
 @app.route('/')
 @ssl_required
